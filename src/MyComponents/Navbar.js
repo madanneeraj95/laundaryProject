87c2bcbcde4d653c79bc2119cc32d6ec5faac3f7
madanneeraj <125691528+madanneeraj@users.noreply.github.com>
2023-10-14 23:36:59 +0530

    latest changes

--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Our Project Name
+            LaundaryProject
           </a>
           <button
             className="navbar-toggler"
