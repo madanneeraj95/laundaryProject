2d20e9f429ec539201fd6eeac5a50e9cad6c0cd2
madanneeraj <125691528+madanneeraj@users.noreply.github.com>
2023-10-14 23:32:09 +0530

    neeraj
    neeraj

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Dashborad from "./MyComponents/Dashborad";
+// import Footer from "./MyComponents/Footer";
+import Navbar from "./MyComponents/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <Dashborad />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div> my Footer Works</div>;
+};
+
+export default Footer;
